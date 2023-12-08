5106a11d41a7f0464e2b83ab707b1105384364c7
Brandon Nguyen <brandonxnguyen@gmail.com>
2023-12-08 14:43:54 -0800

    Created basic server structure

--- api/server.js
+++ api/server.js
@@ -1,3 +1,16 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express')
+
+const server = express(); 
+
+server.use(express.json()) //ability to read JSON data
+
+server.get('/api/users', (req, res) => {
+    res.status(200).json({
+        id: "a_unique_id", // String, required
+        name: "Jane Doe",  // String, required
+        bio: "Having fun", // String, required
+      })
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const server = require('./api/server');
 
 const port = 9000;
 
+server.listen(port, () => {
+    console.log(`server started on http://localhost:${port}`)
+})
+
+
+
 // START YOUR SERVER HERE
