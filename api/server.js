b7d0afe0ee387b175b745b29f025970b9747221b
Brandon Nguyen <brandonxnguyen@gmail.com>
2023-12-08 15:34:54 -0800

    fix the code for when code is not found

--- api/server.js
+++ api/server.js
@@ -10,9 +10,8 @@ server.get('/api/users', (req, res) => {
         res.status(200).json({users})
     })
     .catch(err => {
-        res.status(500).json({
-            message: 'error getting users',
-            err: err.message,
+        res.status(500).json({ 
+            message: "The users information could not be retrieved" 
         })
     })
 })
@@ -20,15 +19,31 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
     .then(user => {
-        res.status(200).json({user})
+        if(!user) {
+            res.status(404).json({ 
+                message: "The user with the specified ID does not exist" 
+            })
+        } else {
+            res.status(200).json({user})
+        }
     })
     .catch(err => {
-        res.status(500).json({
-            message: 'error getting users by ID',
-            err: err.message,
+        res.status(500).json({ 
+            message: "The user information could not be retrieved" 
         })
     })
 })
+
+// server.post('/api/users', (req, res) => {
+//     User.insert((req.params.name, req.params.bio))
+//     .then()
+//     .catch(err => {
+//         res.status(500).json({
+//             message: 'error creating user',
+//             err: err.message,
+//         })
+//     })
+// })
 
 server.use(express.json()) //ability to read JSON data
 
