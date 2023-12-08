56e349e85770fe72820ffdc898f7874bc1dbb87f
Brandon Nguyen <brandonxnguyen@gmail.com>
2023-12-08 14:54:36 -0800

    fixed User's path

--- api/server.js
+++ api/server.js
@@ -1,16 +1,23 @@
 // BUILD YOUR SERVER HERE
 
 const express = require('express')
-
+const User = require('./users/model')
 const server = express(); 
+
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        throw new Error('LOL RIP')
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+        })
+    })
+})
 
 server.use(express.json()) //ability to read JSON data
 
-server.get('/api/users', (req, res) => {
-    res.status(200).json({
-        id: "a_unique_id", // String, required
-        name: "Jane Doe",  // String, required
-        bio: "Having fun", // String, required
-      })
-})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
